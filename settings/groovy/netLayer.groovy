31a81b7673a7e8f812b8a7e252142132e9e0ee83
Borovec Ondrej <devnull@localhost>
2014-07-14 17:39:43 +0200

    Vizualization - better looking map

---
+++ settings/groovy/netLayer.groovy
@@ -0,0 +1,14 @@
+netLayer {
+	lane {
+		view = true;
+		width = 10;
+	}
+	edge {
+		view = true;
+		width = 1;
+	}
+	crossRoad {
+		view = false;
+		width = 1;
+	}
+}
--- src/main/java/cz/agents/highway/vis/NetLayer.java
+++ src/main/java/cz/agents/highway/vis/NetLayer.java
@@ -1,22 +1,18 @@
 package cz.agents.highway.vis;
 
+import cz.agents.alite.configreader.ConfigReader;
+import cz.agents.alite.configurator.Configurator;
 import cz.agents.alite.vis.Vis;
-import cz.agents.alite.vis.element.*;
-import cz.agents.alite.vis.layer.AbstractLayer;
 import cz.agents.alite.vis.layer.GroupLayer;
 import cz.agents.alite.vis.layer.VisLayer;
-import cz.agents.alite.vis.layer.terminal.LineLayer;
-import cz.agents.alite.vis.layer.terminal.PointLayer;
 import cz.agents.highway.environment.roadnet.Edge;
 import cz.agents.highway.environment.roadnet.Junction;
 import cz.agents.highway.environment.roadnet.Lane;
 import cz.agents.highway.environment.roadnet.Network;
 import java.awt.*;
-import java.awt.Point;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-import java.util.LinkedList;
 import javax.vecmath.Point2f;
 import javax.vecmath.Vector2f;
 
@@ -29,11 +25,14 @@ public class NetLayer extends GroupLayer implements VisLayer {
     
     final int LANE_WIGTH = 10;
     final int EDGE_WIGTH = 1;
-    final float MAX_ANGLE = 0.1f;
+    final int CROSSROAD_WIGTH = 1;
+    
     
     final Vector2f VECT_X_NORM = new Vector2f(1, 0);
     final Vector2f VECT_Y_NORM = new Vector2f(0, 1);
     
+    private static final String CONFIG_FILE = "settings/groovy/netLayer.groovy";
+    final private ConfigReader configReader;
     
 
     private Network net;
@@ -42,16 +41,12 @@ public class NetLayer extends GroupLayer implements VisLayer {
 
     public NetLayer(Network roadNetwork) {
         net = roadNetwork;
+        configReader = new ConfigReader();
+        configReader.loadAndMerge(CONFIG_FILE);
+        Configurator.init(configReader);
     }
 
-    void paintLane(Graphics2D canvas, Point2f p1, Point2f p2){
-        if(p1.x != p2.x && p1.y != p2.x){            
-            canvas.setColor(Color.black);
-            canvas.setStroke(new BasicStroke(LANE_WIGTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));            
-            
-            paintLine(canvas, p1, p2);
-        }
-    }
+        
     
     void paintCircle(Graphics2D canvas, Point2f p, int size){
         AffineTransform t = canvas.getTransform();
@@ -84,63 +79,49 @@ public class NetLayer extends GroupLayer implements VisLayer {
     @Override
     public void paint(Graphics2D canvas) {
         super.paint(canvas);
-        
-        for (Lane lane : net.getLanes().values()) {
-            Point2f prev = lane.getShape().get(0);
-            for (Point2f point : lane.getShape()) {
-                paintLane(canvas,point,prev);
-                prev = point;
-            }
-            for (Lane outgoingLane : lane.getOutgoingLanes()) {
-                Point2f point = outgoingLane.getShape().get(0);
-                paintLane(canvas,prev,point);
+        if(configReader.getBoolean("netLayer.lane.view")){
+            canvas.setColor(Color.DARK_GRAY);
+            int size = configReader.getInt("netLayer.lane.width", LANE_WIGTH);
+            canvas.setStroke(new BasicStroke(size, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));  
+            for (Lane lane : net.getLanes().values()) {
+                Point2f prev = lane.getShape().get(0);
+                for (Point2f point : lane.getShape()) {
+                    paintLine(canvas,point,prev);
+                    prev = point;
+                }
+                for (Lane outgoingLane : lane.getOutgoingLanes()) {
+                    Point2f point = outgoingLane.getShape().get(0);
+                    paintLine(canvas,prev,point);
+                }
             }
         }
-    }
+        if(configReader.getBoolean("netLayer.edge.view")){
+            canvas.setColor(Color.red);
+            int size = configReader.getInt("netLayer.edge.width", EDGE_WIGTH);
+            canvas.setStroke(new BasicStroke(size, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+            for (Edge edge : net.getEdges().values()) {
+                if(edge.getShape().isEmpty())continue;
+                Point2f prev = edge.getShape().get(0);
+                for (Point2f point : edge.getShape()) {
+                    paintLine(canvas,point,prev);
+                    prev = point;
+                }
+            }
+        }
+        if(configReader.getBoolean("netLayer.crossRoad.view")){
+            canvas.setColor(Color.green);
+            int size = configReader.getInt("netLayer.crossRoad.width", CROSSROAD_WIGTH);
+            canvas.setStroke(new BasicStroke(size, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+            for (Junction junction : net.getJunctions().values()) {
+                if(junction.getShape().isEmpty())continue;
+                int size2 = junction.getShape().size();
+                Point2f prev = junction.getShape().get(size2-1);
+                for (Point2f point : junction.getShape()) {
+                    paintLine(canvas,point,prev);
+                    prev = point;
+                }
+            }
+        }
     
-//    @Override
-//    public void paint(Graphics2D canvas) {
-//        super.paint(canvas);
-//        int radius = 10;
-//        
-//        canvas.setColor(Color.black);
-//        canvas.setStroke(new BasicStroke(LANE_WIGTH));
-//        for (Lane lane : net.getLanes().values()) {
-//            Point2f prev = lane.getShape().get(0);
-//            for (Point2f point : lane.getShape()) {
-//                paintLane(canvas,point,prev);
-//                prev = point;
-//            }
-//            for (Lane outgoingLane : lane.getOutgoingLanes()) {
-//                Point2f point = outgoingLane.getShape().get(0);
-//                paintLane(canvas,prev,point);
-//            }
-//        }
-//        
-//        canvas.setColor(Color.red);
-//        canvas.setStroke(new BasicStroke(EDGE_WIGTH));
-//        for (Edge edge : net.getEdges().values()) {
-//            if(edge.getShape().isEmpty())continue;
-//            Point2f prev = edge.getShape().get(0);
-//            for (Point2f point : edge.getShape()) {
-//                paintLine(canvas,point,prev);
-//                prev = point;
-//            }
-//        }
-//
-//        canvas.setColor(Color.green);
-//        canvas.setStroke(new BasicStroke(2));
-//
-//        for (Junction junction : net.getJunctions().values()) {
-//
-//            if(junction.getShape().isEmpty())continue;
-//            //
-//            int size = junction.getShape().size();
-//            Point2f prev = junction.getShape().get(size-1);
-//            for (Point2f point : junction.getShape()) {
-//                paintLine(canvas,point,prev);
-//                prev = point;
-//            }
-//        }
-//    }
+    }
 }
