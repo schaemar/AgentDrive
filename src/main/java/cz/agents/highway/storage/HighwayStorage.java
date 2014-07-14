157eb1c27c952a7ea745cf0541fe934e99b554d1
mschaefer [martin schaefer <mschaefer [martin.schaefer@agents.fel.cvut.cz>
2014-07-14 15:42:26 +0200

    Roadobject vis layer

--- src/main/java/cz/agents/highway/storage/HighwayStorage.java
+++ src/main/java/cz/agents/highway/storage/HighwayStorage.java
@@ -50,8 +50,8 @@ public class HighwayStorage extends EventBasedStorage {
     }
 
     public Agent createAgent(final int id) {
-//        Agent agent = new SDAgent(id);
-        Agent agent = new RouteAgent(id);
+        Agent agent = new SDAgent(id);
+//        Agent agent = new RouteAgent(id);
         VehicleSensor sensor = new VehicleSensor(getEnvironment(), agent, this);
         VehicleActuator actuator = new VehicleActuator(getEnvironment(), agent, this);
         agent.addSensor(sensor);
