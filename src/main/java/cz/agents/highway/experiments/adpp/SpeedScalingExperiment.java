25e1572ab23f61f185bfa4a4939f9c5f6401e269
Matej Vavrinec <wmatex@gmail.com>
2014-11-27 11:02:39 +0100

    Initial commit of experiment that scales the number of possible speeds for an agent

---
+++ src/main/java/cz/agents/highway/experiments/adpp/SpeedScalingExperiment.java
@@ -0,0 +1,29 @@
+package cz.agents.highway.experiments.adpp;
+
+import cz.agents.highway.experiments.Experiment;
+import org.apache.commons.cli.CommandLine;
+
+/**
+ * Experiment scaling the number of speeds
+ * Created by wmatex on 26.11.14.
+ */
+public class SpeedScalingExperiment extends Experiment {
+    /**
+     * Run the experiment with the scaling quality
+     *
+     * @param quality Current value for the measured quality
+     * @return
+     */
+    @Override
+    public boolean run(double quality, boolean verbose) {
+        return false;
+    }
+
+    /**
+     * Initialize the experiment
+     */
+    @Override
+    protected void init() {
+
+    }
+}
