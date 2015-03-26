5cba2845e934b2200e4267514be56dd86d99dd75
Matej Vavrinec <wmatex@gmail.com>
2015-03-26 23:06:00 +0100

    Prepare the ADPP agent to be used in general scenario
    - Found trajectories are stored in Highway Storage
    - Priorities are obtained dynamically from PriorityRelation helper class

--- src/main/java/cz/agents/highway/agent/ADPPAgent.java
+++ src/main/java/cz/agents/highway/agent/ADPPAgent.java
@@ -3,6 +3,7 @@ package cz.agents.highway.agent;
 import cz.agents.alite.common.event.Event;
 import cz.agents.alite.vis.VisManager;
 import cz.agents.alite.vis.layer.VisLayer;
+import cz.agents.alite.vis.layer.common.ScreenTextLayer;
 import cz.agents.alite.vis.layer.toggle.KeyToggleLayer;
 import cz.agents.highway.environment.planning.Timer;
 import cz.agents.highway.environment.planning.graph.ControlEffortWrapper;
@@ -24,6 +25,7 @@ import org.jgrapht.util.Goal;
 import org.jgrapht.util.HeuristicToGoal;
 import tt.discrete.Trajectory;
 import tt.discrete.vis.TrajectoryLayer;
+import tt.euclid2d.util.Intersection;
 import tt.euclid2i.Line;
 import tt.euclid2i.Point;
 import tt.euclid2i.trajectory.StraightSegmentTrajectory;
@@ -43,6 +45,7 @@ import tt.vis.ParameterControlLayer;
 import javax.vecmath.Point2f;
 import javax.vecmath.Point3f;
 import java.awt.*;
+import java.io.IOException;
 import java.util.*;
 import java.util.List;
 
@@ -54,30 +57,48 @@ import java.util.List;
  * Created by wmatex on 13.10.14.
  */
 public class ADPPAgent extends Agent {
-    private static final int RADIUS = 5;
+    private static final int RADIUS = 2;
     private static final int SPEED = 1;
     private static final float[] SPEEDS = new float[] {1};
     private static final double WAIT_PENALTY = 0;
     private static final double MOVE_PENALTY = 0;
     private static final int WAIT_DURATION = 1;
-    private static final int MAX_TIME = 10000;
+    private static final int MAX_TIME = 2000;
+    private static final int MAX_PRIORITY = 1000;
     private static final Timer globalTimer = new Timer(true);
     private static final TimeParameter timeParameter = new TimeParameter();
     private static ArrayList<Region> agentTrajectories = new ArrayList<Region>();
+    private static float maxTime = -1;
+    private static int nOfReplans = 0;
 
     DirectedGraph<Point, Line> spatialGraph;
     DirectedGraph<tt.euclidtime3i.Point, Straight> planningGraph;
+
+    private PriorityRelation priorityRelation;
 
     Timer timer = new Timer(false);
 
     tt.euclid2i.Trajectory trajectory;
     VisLayer trajectoryLayer = null;
+    VisLayer movingRegionLayer = null;
     Point start, goal;
     long expanded = 0;
     static long notFound = 0;
+    String heuristic;
+    float[] speeds;
+    double waitPenalty;
+    double movePenalty;
+    int waitDuration;
+    int lastTime = 0;
+
+    int speed = 0;
+
+    boolean needsReplan = true;
+
+    private Map<Integer, Region> movingObstaclesSaved = new LinkedHashMap<Integer, Region>();
 
     // Should we print additional information?
-    boolean verbose = true;
+    boolean verbose = false;
 
     public ADPPAgent(int id) {
         this(id, SPEEDS, WAIT_PENALTY, MOVE_PENALTY, "perfect", WAIT_DURATION, true, true, false);
@@ -87,6 +108,15 @@ public class ADPPAgent extends Agent {
                      String heuristic, int waitDuration, boolean vis, boolean verbose, boolean emptyTrajectories) {
         super(id);
         this.verbose = verbose;
+        this.heuristic = heuristic;
+        this.speeds = speeds;
+        this.waitPenalty = waitPenalty;
+        this.movePenalty = movePenalty;
+        this.waitDuration = waitDuration;
+
+//        this.speed = (int) (Math.random() * 3 + 1);
+        this.speed = 1;
+        System.out.println("Agent " + id + ", speed = "+speed);
 
         // Empty agent trajectories
         if (emptyTrajectories) {
@@ -97,7 +127,7 @@ public class ADPPAgent extends Agent {
         spatialGraph = RoadNetWrapper.create(navigator.getUniqueLaneIndex());
         VisLayer graphLayer;
 
-        if (id == 0 && vis) {
+        if (id == 1 && vis) {
             VisManager.registerLayer(ParameterControlLayer.create(timeParameter));
             graphLayer = KeyToggleLayer.create("g", true, GraphLayer.create(new GraphLayer.GraphProvider<Point, Line>() {
                 @Override
@@ -107,48 +137,22 @@ public class ADPPAgent extends Agent {
             }, new ProjectionTo2d(), Color.BLUE, Color.RED, 2, 3));
             VisManager.registerLayer(graphLayer);
         }
-        print("Building graph: "+timer.getElapsedTime());
-
-        Edge lastEdge = navigator.getRoute().get(navigator.getRoute().size()-1);
-        Iterator<Lane> laneIterator = lastEdge.getLanes().values().iterator();
-        for (int i = 0; i < id; i++) {
-            navigator.changeLaneLeft();
-            laneIterator.next();
-        }
-        Lane l = laneIterator.next();
-        List<Point2f> innerPs = l.getInnerPoints();
-        Point2f lastPoint = innerPs.get(innerPs.size()-1);
-        goal = new Point(Math.round(lastPoint.x), Math.round(lastPoint.y));
-        start = new Point(Math.round(navigator.getRoutePoint().x), Math.round(navigator.getRoutePoint().y));
-
-        print("Agent "+id+" planning");
-//        planningGraph = new ConstantSpeedTimeExtension(spatialGraph, MAX_TIME, new int[] {speed}, new ArrayList<Region>(agentTrajectories), 1, 1);
-        planningGraph = new ConstantSpeedTimeExtension(spatialGraph, MAX_TIME, speeds, new ArrayList<Region>(agentTrajectories), waitDuration, 1);
-//        planningGraph = new ConstantSpeedTimeExtension(spatialGraph, MAX_TIME, SPEEDS, new ArrayList<Region>(), 1, 1);
-        planningGraph = new FreeOnTargetWaitExtension(planningGraph, goal);
-        planningGraph = new ControlEffortWrapper(planningGraph, movePenalty, waitPenalty);
-        // Do the planning
-        this.plan(heuristic);
-        VisManager.registerLayer(KeyToggleLayer.create(""+id, true, tt.euclidtime3i.vis.RegionsLayer.create(new tt.euclidtime3i.vis.RegionsLayer.RegionsProvider() {
-
-            @Override
-            public Collection<Region> getRegions() {
-                List<Region> regions = new ArrayList<Region>(1);
-                regions.add(new MovingCircle(trajectory, RADIUS));
-                return regions;
-            }
-        }, new TimeParameterProjectionTo2d(timeParameter), AgentColors.getColorForAgent(id), AgentColors.getColorForAgent(id))));
-        print("Sum time: "+globalTimer.getElapsedTime());
     }
 
 
     public void addSensor(final VehicleSensor sensor) {
         this.sensor = sensor;
+        priorityRelation = new TrafficRulesPriorityRelation(sensor);
         this.sensor.registerReaction(new Reaction() {
             @Override
             public void react(Event event) {
                 if (event.getType().equals(HighwayEventType.UPDATED)) {
                     actuator.act(agentReact());
+                } else if (event.getType().equals(HighwayEventType.TRAJECTORY_CHANGED)) {
+                    int key = (Integer) event.getContent();
+                    if (priorityRelation.higherPriority().contains(key)) {
+                        needsReplan = true;
+                    }
                 }
             }
         });
@@ -163,16 +167,56 @@ public class ADPPAgent extends Agent {
         RoadObject me = sensor.senseCurrentState(); // my current state
         // Simulator did not send update yet
         int time = (int) Math.floor(me.getUpdateTime() / 1000);
-        if (me == null || trajectory == null) {
+        if (me == null) {
             actions.add(new WPAction(id, 0d, getInitialPosition(), 0));
         } else {
+            if (needsReplan) {
+                Map<Integer, Region> trajectories = sensor.senseTrajectories();
+                List<Integer> higherPriority = priorityRelation.higherPriority();
+                for (int id: higherPriority) {
+                    Region t = trajectories.get(id);
+                    if (t != null) {
+                        movingObstaclesSaved.put(id, t);
+                    }
+                }
+                System.out.println("AgentID: " + id);
+//                planningGraph = new ConstantSpeedTimeExtension(spatialGraph, MAX_TIME, new int[] {speed}, new ArrayList<Region>(agentTrajectories), 1, 1);
+                planningGraph = new ConstantSpeedTimeExtension(spatialGraph, MAX_TIME, new int[]{speed}, movingObstaclesSaved.values(), waitDuration, 1);
+//                planningGraph = new ConstantSpeedTimeExtension(spatialGraph, MAX_TIME, SPEEDS, new ArrayList<Region>(), 1, 1);
+                //planningGraph = new FreeOnTargetWaitExtension(planningGraph, goal);
+                planningGraph = new ControlEffortWrapper(planningGraph, movePenalty, waitPenalty);
+                // Do the planning
+                this.plan(heuristic);
+                needsReplan = false;
+                if (trajectory != null) {
+                    lastTime = time;
+                    if (movingRegionLayer != null) {
+                        VisManager.unregisterLayer(movingRegionLayer);
+                    }
+                    movingRegionLayer = KeyToggleLayer.create("" + id, true, tt.euclidtime3i.vis.RegionsLayer.create(new tt.euclidtime3i.vis.RegionsLayer.RegionsProvider() {
 
-            Point p = trajectory.get(time);
-            if (p == null) {
-                p = trajectory.get(trajectory.getMaxTime());
+                        @Override
+                        public Collection<Region> getRegions() {
+                            List<Region> regions = new ArrayList<Region>(1);
+                            regions.add(new MovingCircle(trajectory, RADIUS));
+                            return regions;
+                        }
+                    }, new TimeParameterProjectionTo2d(timeParameter), AgentColors.getColorForAgent(id), AgentColors.getColorForAgent(id)));
+                    VisManager.registerLayer(movingRegionLayer);
+                }
             }
-            Point3f planP = new Point3f(p.x, p.y, 0);
-            actions.add(new WPAction(me.getId(), me.getUpdateTime(), planP, SPEED));
+
+            if (trajectory == null) {
+                actions.add(new WPAction(id, 0d, getInitialPosition(), 0));
+            } else {
+                Point p = trajectory.get(time);
+                if (p == null) {
+                    //needsReplan = true;
+                    p = trajectory.get(trajectory.getMaxTime());
+                }
+                Point3f planP = new Point3f(p.x, p.y, 0);
+                actions.add(new WPAction(me.getId(), me.getUpdateTime(), planP, speed));
+            }
         }
         // Replan every 10 seconds
         if (time > 0 && time % 10 == 0) {
@@ -187,6 +231,18 @@ public class ADPPAgent extends Agent {
      * Plan the optimal non-collision trajectory
      */
     private void plan(String heuristic) {
+        if (trajectoryLayer != null) {
+            VisManager.unregisterLayer(trajectoryLayer);
+        }
+        Edge lastEdge = navigator.getRoute().get(navigator.getRoute().size()-1);
+        Iterator<Lane> laneIterator = lastEdge.getLanes().values().iterator();
+        Lane l = laneIterator.next();
+        List<Point2f> innerPs = l.getInnerPoints();
+        Point2f lastPoint = innerPs.get(innerPs.size()-1);
+        Point3f current = sensor.senseCurrentState().getPosition();
+        Point2f routePoint = navigator.getRoutePoint();
+        start = new Point(Math.round(routePoint.x), Math.round(routePoint.y));
+        goal = new Point(Math.round(lastPoint.x), Math.round(lastPoint.y));
         print("Start: "+start);
         print("Goal: " + goal);
         expanded = 0;
@@ -212,10 +268,18 @@ public class ADPPAgent extends Agent {
                 public boolean isGoal(tt.euclidtime3i.Point point) {
                     ++expanded;
 //                    print("Trying: "+point);
+                    //return (point.getTime() >= MAX_TIME || goal.distance(point.getPosition()) < 1);
                     return (goal.distance(point.getPosition()) < 1);
                 }
             });
-        print("Planning took: " + timer.getElapsedTime()+", expanded nodes: "+expanded);
+        float planTime = timer.getRawElapsedTime();
+        if (planTime > maxTime) {
+            maxTime = planTime;
+        }
+        nOfReplans++;
+        print("Planning took: " + timer.getElapsedTime() + ", expanded nodes: " + expanded);
+        print("Maximal time: "+maxTime+", number of replans: "+nOfReplans);
+
         if (path == null) {
             trajectory = null;
             ++notFound;
@@ -225,18 +289,17 @@ public class ADPPAgent extends Agent {
         print("Trajectory end time: "+path.getEndVertex().getTime());
         trajectory = new StraightSegmentTrajectory(path, path.getEndVertex().getTime());
         MovingCircle region = new MovingCircle(trajectory, RADIUS);
-        try {
-            agentTrajectories.set(id, region);
-        } catch (IndexOutOfBoundsException e) {
-            agentTrajectories.add(region);
-        }
-        trajectoryLayer = TrajectoryLayer.create(new TrajectoryLayer.TrajectoryProvider<Point>() {
+
+        // Dispatch an event
+        actuator.getEventProcessor().addEvent(HighwayEventType.TRAJECTORY_UPDATED, null, null,
+                new AbstractMap.SimpleEntry<Integer, Region>(this.id, region));
+/*        trajectoryLayer = TrajectoryLayer.create(new TrajectoryLayer.TrajectoryProvider<Point>() {
             @Override
             public Trajectory<Point> getTrajectory() {
                 return trajectory;
             }
         }, new ProjectionTo2d(), AgentColors.getColorForAgent(id), 1, trajectory.getMaxTime(), 't');
-        VisManager.registerLayer(trajectoryLayer);
+        VisManager.registerLayer(trajectoryLayer);*/
     }
 
     /**
--- src/main/java/cz/agents/highway/storage/HighwayStorage.java
+++ src/main/java/cz/agents/highway/storage/HighwayStorage.java
@@ -13,6 +13,9 @@ import cz.agents.alite.common.event.Event;
 import cz.agents.alite.environment.eventbased.EventBasedStorage;
 import cz.agents.alite.simulation.SimulationEventType;
 import cz.agents.highway.storage.plan.Action;
+import tt.euclid2i.Trajectory;
+import tt.euclidtime3i.Region;
+import tt.euclidtime3i.region.MovingCircle;
 
 public class HighwayStorage extends EventBasedStorage {
 
@@ -22,6 +25,7 @@ public class HighwayStorage extends EventBasedStorage {
     private final Map<Integer, Agent> agents = new LinkedHashMap<Integer, Agent>();
     private final Map<Integer, RoadObject> posCurr = new LinkedHashMap<Integer, RoadObject>();
     private final Map<Integer, Action> actions = new LinkedHashMap<Integer, Action>();
+    private final Map<Integer, Region> trajectories = new LinkedHashMap<Integer, Region>();
 
     private Agent queen;
 
@@ -41,6 +45,15 @@ public class HighwayStorage extends EventBasedStorage {
             logger.debug("HighwayStorage: handled: RADAR_DATA");
             RadarData radar_data = (RadarData) event.getContent();
             updateCars(radar_data);
+        } else if (event.isType(HighwayEventType.TRAJECTORY_UPDATED)) {
+            Map.Entry<Integer, Region> agentTrajectory = (Map.Entry<Integer, Region>) event.getContent();
+            MovingCircle stored = (MovingCircle) trajectories.get(agentTrajectory.getKey());
+            MovingCircle inc    = (MovingCircle) agentTrajectory.getValue();
+            if (stored == null || !stored.getTrajectory().equals(inc.getTrajectory())) {
+                trajectories.put(agentTrajectory.getKey(), agentTrajectory.getValue());
+                logger.debug("Changed trajectory of agent: "+agentTrajectory.getKey());
+                getEnvironment().getEventProcessor().addEvent(HighwayEventType.TRAJECTORY_CHANGED, null, null, agentTrajectory.getKey());
+            }
         }
 
     }
@@ -62,13 +75,13 @@ public class HighwayStorage extends EventBasedStorage {
             agent = new RouteAgent(id);
         } else if (agentClassName.equals("SDAgent")) {
             agent = new SDAgent(id);
-        } else if (agentClassName.equals("ORCAAgent")) {
-            agent = new ORCAAgent(id);
+        /*} else if (agentClassName.equals("ORCAAgent")) {
+            agent = new ORCAAgent(id);*/
         } else if (agentClassName.equals("ADPPAgent")) {
             agent = new ADPPAgent(id);
-        }/*else if (agentClassName.equals("test")) {
+        /*} else if (agentClassName.equals("test")) {
             agent = new testAgent(id);
-        }*/
+        */
         } else if (agentClassName.equals("GSDAgent")) {
             agent = new GSDAgent(id,(HighwayEnvironment)getEnvironment());
         }
@@ -106,6 +119,10 @@ public class HighwayStorage extends EventBasedStorage {
         return actions;
     }
 
+    public Map<Integer, Region> getTrajectories() {
+        return trajectories;
+    }
+
     public void updateCars(RadarData object) {
         if(!object.getCars().isEmpty()) {
             for (RoadObject car : object.getCars()) {
@@ -119,6 +136,7 @@ public class HighwayStorage extends EventBasedStorage {
     {
         agents.remove(carID);
     }
+
 
 //    public void updateInit(InitIn init) {
 //        getRoadDescription().addPoints(init.getPoints());
