a99c6602e04edba796ada13efc3f6ce1db97abac
Matej Vavrinec <wmatex@gmail.com>
2015-04-30 16:05:04 +0200

    Added new options for the ADPP agent

--- settings/groovy/local/wmatex.groovy
+++ settings/groovy/local/wmatex.groovy
@@ -5,6 +5,7 @@
 highway {
     //agent = "RouteAgent";
     agent = "ADPPAgent";
+//    agent = "GSDAgent";
     rvo {
         agent {
             randomRoutes = false;
@@ -22,9 +23,27 @@ highway {
 //        folder = "nets/test90";
 //        folder = "nets/super-collision";
 //        folder = "nets/artificialX-junction";
+//        folder = "nets/artificialT-junction";
 //        folder = "nets/artificialX-junction-smaller";
 //        folder = "nets/artificialHighway-funnel";
-        folder = "nets/simple"
+//        folder = "nets/simple"
+
+        folder = "nets/experiments/X-junction";
+
+        lane {
+            stepSize = 2f;
+        }
+    }
+
+    ADPPAgent {
+        radius = 2;           //[m]
+        waitPenalty = 0.1d;
+        movePenalty = 0.01d;
+        waitDuration = 1d;    //[s]
+        maxSpeed = 5d;        //[m/s]
+        maxAcceleration = 2d; //[m/s^2]
+        planningHorizon = 10; //[s]
+        planProlongationFactor = 3;
     }
 
     dashboard {
@@ -41,6 +60,10 @@ highway {
         }
 
         simulatorsToRun = [];
+        sumoSimulation = true;
+        numberOfCarsInSimulation = 50;
+        //numberOfCarsInSimulation = agents.size();
+
     }
 }
 
