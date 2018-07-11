b457cdb0ebccfb65c616ac5e8d2aac3f0f260b80
Jan Kalina <jankalina135@gmail.com>
2018-07-11 21:08:34 +0200

    Added Mlada Boleslav and Skoda parking networks

---
+++ data/nets/generatorWin.bat
@@ -0,0 +1,3 @@
+netconvert   --osm-files "%1\%1.osm" -o "%1\%1.net.xml" --tls.join --no-internal-links --no-turnarounds true --proj "+proj=utm +ellps=bessel +units=m"
+python "%SUMO_HOME%tools/randomTrips.py" -n "%1\%1.net.xml" -e 50 -l -o "%1\%1.trips.xml"
+duarouter --trip-files="%1\%1.trips.xml" --net="%1\%1.net.xml"  --output-file="%1\%1.rou.xml" --ignore-errors --routing-algorithm=astar
